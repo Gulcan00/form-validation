0a422d96164ff3dd3d8f5b14305cff78ce8c2cef
Gulcan Damdelen <61693319+Gulcan00@users.noreply.github.com>
2023-12-12 16:40:55 +0200

    Update form validation error messages

--- script.js
+++ script.js
@@ -1,32 +1,53 @@
-function showError(field) {
+function showError(field, name) {
     const fieldId = field.id;
-    const errorSpan = document.querySelector(`label[for="${fieldId}"] span.error`);
+    const errorSpan = document.querySelector(`label[for="${fieldId}"] span`);
+    errorSpan.classList.add('error');
     
     if (field.validity.valueMissing) {
-        errorSpan.innerText = `Please enter your ${fieldId}`;
+        errorSpan.innerText = `Please enter your ${name}`;
     } else if (field.validity.typeMismatch) {
-        errorSpan.innerText = `Please enter a valid ${fieldId}`;
+        errorSpan.innerText = `Please enter a valid ${name}`;
     } else if (field.validity.tooShort) {
-        errorSpan.innerText = `Please enter a ${fieldId} with at least ${field.minLength} characters`;
+        errorSpan.innerText = `Please enter a ${name} with at least ${field.minLength} characters`;
     } else {
         errorSpan.innerText = '';
+        errorSpan.classList.remove('error');
     }
 }
 
 function removeError(field) {
     const errorSpan = document.querySelector(`label[for="${field}"] span.error`);
     errorSpan.innerText = '';
+    errorSpan.classList.remove('error');
 }
 
 function validateForm() {
     const form = document.querySelector('form');
     const email = document.querySelector('#email');
+    const country = document.querySelector('#country');
+    const zipCode = document.querySelector('#zipCode');
 
     email.addEventListener('input', () => {
         if (email.checkValidity()) {
             removeError('email');
         } else {
-            showError(email);
+            showError(email, 'email');
+        }
+    });
+
+    country.addEventListener('input', () => {
+        if (country.checkValidity()) {
+            removeError('country');
+        } else {
+            showError(country, 'country');
+        }
+    });
+
+    zipCode.addEventListener('input', () => {
+        if (zipCode.checkValidity()) {
+            removeError('zipCode');
+        } else {
+            showError(zipCode, 'zip code');
         }
     });
 
@@ -35,7 +56,9 @@ function validateForm() {
         if (form.checkValidity()) {
             alert('Form submitted');
         } else {
-            showError(email);
+            showError(email, 'email');
+            showError(country, 'country');
+            showError(zipCode, 'zip code');
         }
     })
 }
