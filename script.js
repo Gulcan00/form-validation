74abd68d69a0a8d38b873ec5c7628415db521968
Gulcan Damdelen <61693319+Gulcan00@users.noreply.github.com>
2023-12-14 09:18:24 +0200

    Refactor password validation in showError function

--- script.js
+++ script.js
@@ -3,22 +3,72 @@ function showError(field, name) {
     const errorSpan = document.querySelector(`label[for="${fieldId}"] span`);
     errorSpan.classList.add('error');
     
-    if (field.validity.valueMissing) {
+    if (fieldId === 'password') {
+        errorSpan.innerHTML = null;
+        errorSpan.classList.remove('error');
+        const uppercaseSpan = document.createElement('span');
+        uppercaseSpan.classList.add('password');
+        const lowercaseSpan = document.createElement('span');
+        uppercaseSpan.classList.add('password');
+        const numberSpan = document.createElement('span');
+        numberSpan.classList.add('password');
+        const spaceSpan = document.createElement('span');
+        spaceSpan.classList.add('password');
+        const lengthSpan = document.createElement('span');
+        lengthSpan.classList.add('password');
+        const emptySpan = document.createElement('span');
+        emptySpan.classList.add('password');
+
+        if (field.validity.valueMissing) {
+            emptySpan.classList.add('error');
+            emptySpan.innerText = `Please enter your ${name}`;
+        } else {
+            emptySpan.classList.remove('password');
+        }
+        if (field.validity.tooShort) {
+            lengthSpan.classList.add('error');
+            lengthSpan.innerText = `Please enter a ${name} with at least ${field.minLength} characters`;
+        } else {
+            lengthSpan.classList.remove('password');
+        }
+        if (!/[A-Z]/.test(field.value)) {
+            uppercaseSpan.classList.add('error');
+            uppercaseSpan.innerText = 'At least one uppercase letter';
+        } else {
+            uppercaseSpan.classList.remove('password');
+        }
+        if (!/[a-z]/.test(field.value)) {
+            lowercaseSpan.classList.add('error');
+            lowercaseSpan.innerText = 'At least one lowercase letter';
+        } else {
+            lowercaseSpan.classList.remove('password');
+        }
+        if (!/[0-9]/.test(field.value)) {
+            numberSpan.classList.add('error');
+            numberSpan.innerText = 'At least one number';
+        } else {
+            numberSpan.classList.remove('password');
+        }
+        if (/[\s]/.test(field.value)) {
+            spaceSpan.classList.add('error');
+            spaceSpan.innerText = 'No spaces allowed';
+        } else {
+            spaceSpan.classList.remove('password');
+        }
+
+        errorSpan.appendChild(lengthSpan);
+        errorSpan.appendChild(emptySpan);
+        errorSpan.appendChild(uppercaseSpan);
+        errorSpan.appendChild(lowercaseSpan);
+        errorSpan.appendChild(numberSpan);
+        errorSpan.appendChild(spaceSpan);
+
+    } else if (field.validity.valueMissing) {
         errorSpan.innerText = `Please enter your ${name}`;
     } else if (field.validity.typeMismatch) {
         errorSpan.innerText = `Please enter a valid ${name}`;
     } else if (field.validity.tooShort) {
         errorSpan.innerText = `Please enter a ${name} with at least ${field.minLength} characters`;
-    } else if (field.validity.patternMismatch && fieldId === 'password') {
-        if (!/[A-Z]/.test(field.value)) {
-            errorSpan.innerText = `Please enter a ${name} with at least one uppercase letter`;
-        } else if (!/[a-z]/.test(field.value)) {
-            errorSpan.innerText = `Please enter a ${name} with at least one lowercase letter`;
-        } else if (!/[0-9]/.test(field.value)) {
-            errorSpan.innerText = `Please enter a ${name} with at least one number`;
-        } else if (/[\s]/.test(field.value)) {
-            errorSpan.innerText = `Please enter a ${name} without spaces`;
-        }
     } else {
         errorSpan.innerText = '';
         errorSpan.classList.remove('error');
